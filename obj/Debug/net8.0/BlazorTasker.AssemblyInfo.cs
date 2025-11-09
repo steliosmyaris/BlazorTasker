fabc3e08dd90ecb704004e18f0c8cb1c355a6a77
steliosmyaris <steliosmyaris@gmail.com>
2025-11-09 16:06:04 +0200

    Final

--- obj/Debug/net8.0/BlazorTasker.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorTasker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc3a6e93ac908626ed2d905add11a0d8f74048f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62cf225211e7cd012f43ca15300c79b679b6cc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
